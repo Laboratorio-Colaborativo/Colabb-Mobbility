e9611a462b0ccdbd2a19f4387924019d1cc84d93
johan ardila <johan321q@gmail.com>
2025-12-06 17:24:42 -0500

    Proyecto inicial + configuracion de cognito

---
+++ android/app/src/main/kotlin/com/example/colabb_mobility/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.colabb_mobility
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,15 @@
+const amplifyconfig = '''{
+  "auth": {
+    "plugins": {
+      "awsCognitoAuthPlugin": {
+        "CognitoUserPool": {
+          "Default": {
+            "PoolId": "us-east-2_a876CFQP6",
+            "AppClientId": "17dm9n6rpclasad1aj18mucskl",
+            "Region": "us-east-2"
+          }
+        }
+      }
+    }
+  }
+}''';
